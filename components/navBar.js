d723e1d7e019a88d71af446cfb23dd15a02b72b7
adamknapp31 <adamknapp31@gmail.com>
2024-04-13 08:23:48 -0400

    reverted nav bar back to bootstrap 4

--- components/navBar.js
+++ components/navBar.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function(){
                 <a class="navbar-brand" href="index.html" style="color: #FFFFFF;">
                     <img src="images/FarmLogo.svg" alt="Farm logo showing the brand identity" style="max-height: 80px; filter: brightness(0) invert(1);">
                 </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" style="border-color: rgba(255, 255, 255, 0.5);">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" style="border-color: rgba(255, 255, 255, 0.5);">
                     <span class="navbar-toggler-icon" style="background-image: url('data:image/svg+xml;charset=utf8,%3Csvg viewBox=\'0 0 30 30\' xmlns=\'http://www.w3.org/2000/svg\'%3E%3Cpath stroke=\'rgba(255, 255, 255, 0.8)\' stroke-width=\'2\' stroke-linecap=\'round\' stroke-miterlimit=\'10\' d=\'M4 7h22M4 15h22M4 23h22\'/%3E%3C/svg%3E');"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
